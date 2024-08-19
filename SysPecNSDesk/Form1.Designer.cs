a420ca28e76fadd353f6c26483098f2c92ce1ce9
igor.omelo1 <igor.omelo1@ITQ0675999W10-1>
2024-08-19 09:08:28 -0300

    Adicionar arquivos de projeto.

---
+++ SysPecNSDesk/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SysPecNSDesk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNumero1 = new TextBox();
+            lstResultado = new ListBox();
+            btnConfirmar = new Button();
+            cmbOperacao = new ComboBox();
+            txtNumero2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            txtNumero1.Location = new Point(153, 144);
+            txtNumero1.Name = "txtNumero1";
+            txtNumero1.Size = new Size(100, 23);
+            txtNumero1.TabIndex = 1;
+            txtNumero1.TextChanged += txtNumero1_TextChanged;
+            // 
+            // lstResultado
+            // 
+            lstResultado.FormattingEnabled = true;
+            lstResultado.ItemHeight = 15;
+            lstResultado.Location = new Point(154, 173);
+            lstResultado.Name = "lstResultado";
+            lstResultado.Size = new Size(307, 199);
+            lstResultado.TabIndex = 2;
+            lstResultado.SelectedIndexChanged += lstResultado_SelectedIndexChanged;
+            // 
+            // btnConfirmar
+            // 
+            btnConfirmar.Enabled = false;
+            btnConfirmar.Location = new Point(386, 144);
+            btnConfirmar.Name = "btnConfirmar";
+            btnConfirmar.Size = new Size(75, 23);
+            btnConfirmar.TabIndex = 3;
+            btnConfirmar.Text = "Confirmar";
+            btnConfirmar.UseVisualStyleBackColor = true;
+            btnConfirmar.Click += btnConfirmar_Click;
+            // 
+            // cmbOperacao
+            // 
+            cmbOperacao.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbOperacao.FormattingEnabled = true;
+            cmbOperacao.Items.AddRange(new object[] { "Multiplicação", "Divisão", "Adição", "Subtração", "Exponenciação", "Radiciação", "Logaritmos" });
+            cmbOperacao.Location = new Point(154, 89);
+            cmbOperacao.Name = "cmbOperacao";
+            cmbOperacao.Size = new Size(121, 23);
+            cmbOperacao.TabIndex = 4;
+            cmbOperacao.SelectedIndexChanged += cmbOperacao_SelectedIndexChanged;
+            // 
+            // txtNumero2
+            // 
+            txtNumero2.Location = new Point(270, 144);
+            txtNumero2.Name = "txtNumero2";
+            txtNumero2.Size = new Size(100, 23);
+            txtNumero2.TabIndex = 5;
+            txtNumero2.TextChanged += txtNumero2_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(154, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Operação";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(153, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Valor 1";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(270, 126);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Valor 2";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(709, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtNumero2);
+            Controls.Add(cmbOperacao);
+            Controls.Add(btnConfirmar);
+            Controls.Add(lstResultado);
+            Controls.Add(txtNumero1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox txtNumero1;
+        private ListBox lstResultado;
+        private Button btnConfirmar;
+        private ComboBox cmbOperacao;
+        private TextBox txtNumero2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ SysPecNSDesk/Form1.cs
@@ -0,0 +1,151 @@
+using SysPecNSLib;
+using System.Diagnostics.CodeAnalysis;
+namespace SysPecNSDesk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("OLÁ");
+        }
+
+
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            string operacao = cmbOperacao.Text;
+            double posicaoOperacao = cmbOperacao.SelectedIndex;
+            double numero1 = 0;
+            if (txtNumero1.Text != string.Empty)
+            {
+                numero1 = Convert.ToDouble(txtNumero1.Text);
+            }
+
+            double numero2 = 0;
+            double a = 0;
+            
+
+            
+                if (txtNumero2.Text != string.Empty)
+                {
+                    numero2 = Convert.ToDouble(txtNumero2.Text);
+                   
+                    a = numero2;
+
+                }
+
+            
+
+            
+            
+           
+
+            double resultado = 0;
+            switch (posicaoOperacao)
+            {
+                case 0:
+                    resultado = numero1 * numero2;
+                    break;
+                case 1:
+                    resultado = numero1 / numero2;
+                    break;
+                case 2:
+                    resultado = numero1 + numero2;
+                    break;
+                case 3:
+                    resultado = numero1 - numero2;
+                    break;
+                case 4:
+                    resultado = Math.Pow(numero1, numero2);
+                    break;
+                case 5:
+                    resultado = Math.Pow(numero1,1/numero2);
+                    break;
+                case 6:
+                    resultado = Math.Log(numero1,a);
+                    break;
+                default:
+                    break;
+
+
+
+            }
+
+
+
+            //if (posicaoOperacao == 0)
+            //{
+            //    resultado = numero1 * numero2;
+
+            //}
+
+            lstResultado.Items.Add($"{operacao} - {posicaoOperacao} = {resultado}");
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    lstResultado.Items.Add($"{numero} x {i + 1} = {numero * (i + 1)}");
+            //}
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNumero2.Text.Length != 0 && cmbOperacao.SelectedIndex >= 0 && txtNumero1.Text.Length != 0)
+            {
+                btnConfirmar.Enabled = true;
+            }
+            else
+            {
+                btnConfirmar.Enabled = false;
+            }
+
+                    
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNumero1.Text.Length != 0 && cmbOperacao.SelectedIndex >= 0 && txtNumero2.Text.Length != 0)
+            {
+                btnConfirmar.Enabled = true;
+            }         
+            else if (txtNumero1.Text.Length != 0 && cmbOperacao.SelectedIndex == 5 ) 
+            { btnConfirmar.Enabled = true; }
+            else
+            {
+                btnConfirmar.Enabled = false;
+            }
+        }
+
+        private void lstResultado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbOperacao_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+           /* if (cmbOperacao.SelectedIndex !=5)
+            {
+                txtNumero2.Enabled = true;
+            }
+            else
+            {
+                txtNumero2.Enabled = false;
+                
+            }*/
+            txtNumero1.Clear(); 
+            txtNumero2.Clear();
+            txtNumero1.Focus();
+            btnConfirmar.Enabled = false;
+        }
+    }
+}
